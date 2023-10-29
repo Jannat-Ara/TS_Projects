cb86949b0d1c19fafe0ff3b42c6ebd94dab4eaf9
Jannat-Ara <jannat.raya@northsouth.edu>
2023-10-29 15:20:27 +0600

    updating

---
+++ Advanced_shopping_Cart/src/context/ShoppingCartContext.tsx
@@ -0,0 +1,28 @@
+import { createContext ,useContext,ReactNode,useState} from "react"
+
+type ShoppingCartProviderProps={
+    children:ReactNode
+}
+type CartItem={
+    id:number
+    quantity:number
+}
+type ShoppingCartContext={
+    getItemQuantity:(id:number)=>number
+    increaseCartQuantity:(id:number)=>void
+    decreaseCartQuantity:(id:number)=>void
+    removeFromCart:(id:number)=>void
+}
+
+const ShoppingCartContext = createContext({} as ShoppingCartContext)
+export function useShoppingCart(){
+    return useContext(ShoppingCartContext)
+}
+export function ShoppingCartProvider ({children}:ShoppingCartProviderProps){
+    const [cartItem,setCartItems] =useState<CartItem[]>([])
+    return(
+        <ShoppingCartContext.Provider value={{}}>
+            {children}
+        </ShoppingCartContext.Provider>
+    )
+}
--- Advanced_shopping_Cart/src/pages/Store.tsx
+++ Advanced_shopping_Cart/src/pages/Store.tsx
@@ -1,10 +1,18 @@
+
+import { StoreItem } from '../components/StoreItem';
+import storeItems from '../data/items.json';
+
 export default function Store() {
     return (
-        <>
-
-            <div>
-                Store
+        <div className="container mx-auto p-4">
+            <h1 className="text-3xl font-bold mb-4">Store</h1>
+            <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-3 gap-4">
+                {storeItems.map((item, index) => (
+                    <div key={index} className="bg-gray-100 p-4 rounded shadow">
+                        <StoreItem {...item} />
+                    </div>
+                ))}
             </div>
-        </>
-    )
+        </div>
+    );
 }
